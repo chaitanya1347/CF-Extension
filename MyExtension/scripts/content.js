73103575a7baf9abdd1819f54a4bfc9187f0efeb
CHAITANYA AGRAWAL <chaitanyaagrawal7@gmail.com>
2024-07-26 14:33:23 +0530

    Minor Bug Fixed

--- MyExtension/scripts/content.js
+++ MyExtension/scripts/content.js
@@ -1,53 +1,38 @@
-function getHandles() {
-  chrome.storage.local.get('userHandles', function (result) {
-    console.log()
-      if (result.userHandles) {
-        init(result.userHandles);
-      } else {
-        init([]);
-      }
-  });
-}
+async function init(handles) {
+  // ****** Generating the correct api endpoint **********
+  const baseURL = "https://codeforces.com/api/";
 
+  // ****** Generating the HTML for table **********
+  const friendsTableDiv = document.createElement("div");
+  friendsTableDiv.className = "roundbox sidebox top-contributed borderTopRound ";
 
+  const tableCaptionDiv = friendsTableDiv.appendChild(document.createElement("div"));
+  tableCaptionDiv.className = "caption titled";
+  tableCaptionDiv.innerHTML = "Friends' Submissions";
 
-function init(handles){
+  const tableDiv = friendsTableDiv.appendChild(document.createElement("table"));
+  tableDiv.className = "rtable ";
 
-    // ****** Generating the correct api endpoint **********
+  const tableBody = tableDiv.appendChild(document.createElement("tbody"));
+  tableBody.innerHTML = `
+      <tr>
+          <th class="left">User</th>
+          <th>Problem</th>
+          <th class="" style="width:5em;">Time</th>
+      </tr>
+  `;
 
-    let baseURL = "https://codeforces.com/api/";
+  const verdictMap = new Map([
+      ['OK', '✅'],  // Green check mark
+      ['WRONG_ANSWER', '❌'],  // Red cross mark
+      ['TIME_LIMIT_EXCEEDED', '⌛'],  // Hourglass
+      ['MEMORY_LIMIT_EXCEEDED', '💡'],  // Light bulb
+      ['DEFAULT', '⚠️']  // Warning sign
+  ]);
 
-    // ****** Generating the HTML for table **********
-    let friendsTableDiv = document.createElement("div");
-    friendsTableDiv.className = "roundbox sidebox top-contributed borderTopRound ";
+  const getEmoji = (verdict) => verdictMap.get(verdict) ?? verdictMap.get('DEFAULT');
 
-    let tableCaptionDiv = friendsTableDiv.appendChild(document.createElement("div"));
-    tableCaptionDiv.className = "caption titled";
-    tableCaptionDiv.innerHTML = "Friends' Submissions";
-
-    let tableDiv = friendsTableDiv.appendChild(document.createElement("table"));
-    tableDiv.className = "rtable ";
-
-    let tableBody = tableDiv.appendChild(document.createElement("tbody"));
-    tableBody.innerHTML = `
-    <tr>
-        <th class = "left ">User</th>
-        <th>Problem</th>
-        <th class="" style="width:5em;">Time</th>
-    </tr>
-    `;
-    const verdictMap = new Map([
-      ['OK', '✅'],
-      ['WRONG_ANSWER', '❌'],
-      ['TIME_LIMIT_EXCEEDED', '⏱️'],
-      ['MEMORY_LIMIT_EXCEEDED', '💾'],
-      ['DEFAULT', '⚠️']
-    ]);
-
-    const getEmoji = (verdict) => verdictMap.get(verdict) ?? verdictMap.get('DEFAULT');
-
-
-    const ratingMap = new Map([
+  const ratingMap = new Map([
       ['newbie', 'rated-user user-gray'],
       ['pupil', 'rated-user user-green'],
       ['specialist', 'rated-user user-cyan'],
@@ -56,87 +41,86 @@ function init(handles){
       ['master', 'rated-user user-orange'],
       ['international grandmaster', 'rated-user user-red'],
       ['legendary grandmaster', 'rated-user user-legendary']
-    ]);
+  ]);
 
-    console.log(handles);
-    const oneday = 60 * 60 * 24 * 1000; //miliseconds
-    const submissionsCount = 5;
-    let handleCount = 0;
-    handles.forEach(handle => {
-      statusUrl = baseURL + `user.status?handle=${handle}&from=1&count=${submissionsCount}`;
-      // console.log(statusUrl);
-      //Get rating
-      let rating;
-      infoUrl = baseURL + `user.info?handles=${handle}`;
-      // console.log(infoUrl);
-      fetch(infoUrl)
-        .then(res => {
-          if (!res.ok) {
-            throw new Error('Network response was not ok');
+  const oneday = 60 * 60 * 24 * 1000; // milliseconds
+  const submissionsCount = 6;
+  let handleCount = 0;
+
+  for (let handle of handles) {
+      try {
+          // Fetch user info
+          const infoUrl = `${baseURL}user.info?handles=${handle}`;
+          const infoResponse = await fetch(infoUrl);
+          if (!infoResponse.ok) {
+              throw new Error('Network response was not ok');
           }
-          return res.json();
-        })
-        .then(resBody => {
-          console.log(resBody);
-          rating = resBody.result[0].rank;
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+          const infoBody = await infoResponse.json();
+          const rating = infoBody.result[0].rank;
 
-      //Get status of submissions
-      fetch(statusUrl)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
+          // Fetch user submissions
+          const statusUrl = `${baseURL}user.status?handle=${handle}&from=1&count=${submissionsCount}`;
+          const statusResponse = await fetch(statusUrl);
+          if (!statusResponse.ok) {
+              throw new Error('Network response was not ok');
           }
-          return response.json();
-        })
-        .then(responseBody => {
-          for(let i=0; i<submissionsCount; i++){
-            const submissionTime = responseBody.result[i].creationTimeSeconds*1000;
-            if(( (new Date().getTime()) - responseBody.result[i].creationTimeSeconds*1000) < oneday){
-              handleCount++;
+          const responseBody = await statusResponse.json();
 
-              let tableRowElement = document.createElement("tr");
-              if(handleCount%2){
+          // Process submissions
+          for (let i = 0; i < submissionsCount; i++) {
+              const submissionTime = responseBody.result[i].creationTimeSeconds * 1000;
+              if ((new Date().getTime()) - responseBody.result[i].creationTimeSeconds * 1000 < oneday) {
+                  handleCount++;
+
+                  const tableRowElement = document.createElement("tr");
+                  const isOdd = handleCount % 2 !== 0;
+
                   tableRowElement.innerHTML = `
-                      <td class = "left dark"><a class = "${ratingMap.get(rating)}" href="/profile/${handle}"> ${handle} </a> </td>
-                      <td class = " dark" >
-                        <a href = "/problemset/problem/${responseBody.result[i].problem.contestId}/${responseBody.result[i].problem.index}">
-                          ${responseBody.result[i].problem.name}
-                          ${getEmoji(responseBody.result[i].verdict)}
-                        </a>
+                      <td class="left ${isOdd ? 'dark' : ''}">
+                          <a class="${ratingMap.get(rating)}" href="/profile/${handle}">${handle}</a>
                       </td>
-                      <td class = " dark">${new Date(submissionTime).toLocaleTimeString()}</td>
+                      <td class="${isOdd ? 'dark' : ''}">
+                          <a href="/problemset/problem/${responseBody.result[i].problem.contestId}/${responseBody.result[i].problem.index}">
+                              <div style="display: flex; align-items: center;">
+                                  <span style="flex-grow: 1;">${responseBody.result[i].problem.name}</span>
+                                  <span>${getEmoji(responseBody.result[i].verdict)}</span>
+                              </div>
+                          </a>
+                      </td>
+                      <td class="${isOdd ? 'dark' : ''}">${new Date(submissionTime).toLocaleTimeString()}</td>
                   `;
+                  tableBody.appendChild(tableRowElement);
               }
-              else {
-                  tableRowElement.innerHTML = `
-                  <td class = "left"><a class = "${ratingMap.get(rating)}" href="/profile/${handle}"> ${handle} </a></td>
-                  <td class = " ">
-                    <a href = "/problemset/problem/${responseBody.result[i].problem.contestId}/${responseBody.result[i].problem.index}">
-                      ${responseBody.result[i].problem.name}
-                      ${getEmoji(responseBody.result[i].verdict)}
-                    </a>
-                  </td>
-
-                  <td class = " ">${(new Date(submissionTime)).toLocaleTimeString()}</td>
-                  `;
-              }
-              tableBody.appendChild(tableRowElement);
-            }
           }
-        })
-        .catch(error => {
+      } catch (error) {
           console.error('Error:', error);
-        });
-        
-    });
-    
-    let el1 = document.querySelector("#sidebar > div:nth-child(1)");
+      }
+  }
 
-    el1.insertAdjacentElement('afterend', friendsTableDiv);
+  const el1 = document.querySelector("#sidebar > div:nth-child(1)");
+  el1.insertAdjacentElement('afterend', friendsTableDiv);
+}
+
+async function getHandles() {
+  try {
+      const result = await new Promise((resolve, reject) => {
+          chrome.storage.local.get('userHandles', (result) => {
+              if (chrome.runtime.lastError) {
+                  reject(chrome.runtime.lastError.message);
+              } else {
+                  resolve(result.userHandles);
+              }
+          });
+      });
+
+      if (result) {
+          await init(result);
+      } else {
+          init([]);
+      }
+  } catch (error) {
+      console.error('Error:', error);
+  }
 }
 
 getHandles();
